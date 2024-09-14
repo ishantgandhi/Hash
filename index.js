d45b28e778c762efd0804724b3f4a891e9663c0a
ishantgandhi <126364217+ikenakami@users.noreply.github.com>
2024-09-14 16:59:42 +0400

    made routes

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
 const port = 3000;
+const mongoose = require("mongoose");
+const DSA = require("./models/ds.model");
+const Time = require("./models/dstime.model");
+app.use(express.json());
 
-app.get('/lc/:username', async (req, res) => {
-    const username = req.params.username;
+mongoose
+  .connect(
+    "mongodb+srv://igandhi:trisha7102@hash.dyqdd.mongodb.net/Hash?retryWrites=true&w=majority&appName=Hash"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Connection Failed", err);
+  });
 
-    const query = `
+app.get("/lc/:username", async (req, res) => {
+  const username = req.params.username;
+
+  const query = `
     query {
       matchedUser(username: "${username}") {
         username
@@ -25,42 +40,91 @@ app.get('/lc/:username', async (req, res) => {
     }
   `;
 
-    try {
-        const response = await axios.post('https://leetcode.com/graphql', { query });
+  try {
+    const response = await axios.post("https://leetcode.com/graphql", {
+      query,
+    });
 
-        if (!response.data.data || !response.data.data.matchedUser) {
-            return res.status(404).json({ error: 'User not found' });
-        }
-
-        const userProfile = response.data.data.matchedUser.profile;
-        const userAvatar = userProfile.userAvatar;
-        const userRanking = userProfile.ranking;
-        const realName = userProfile.realName;
-        const solvedProblems = response.data.data.matchedUser.submitStats.acSubmissionNum;
-
-
-        const easy = solvedProblems.find(d => d.difficulty === 'Easy').count;
-        const medium = solvedProblems.find(d => d.difficulty === 'Medium').count;
-        const hard = solvedProblems.find(d => d.difficulty === 'Hard').count;
-
-        const result = {
-            userName: realName,
-            profilePic: userAvatar,
-            rank: userRanking,
-            solved: {
-                easy: easy,
-                medium: medium,
-                hard: hard
-            }
-        };
-
-        res.status(200).json(result);
-    } catch (error) {
-        console.error("Error fetching data from LeetCode:", error.response ? error.response.data : error.message);
-        res.status(500).json({ error: 'Error fetching LeetCode data' });
+    if (!response.data.data || !response.data.data.matchedUser) {
+      return res.status(404).json({ error: "User not found" });
     }
+
+    const userProfile = response.data.data.matchedUser.profile;
+    const userAvatar = userProfile.userAvatar;
+    const userRanking = userProfile.ranking;
+    const realName = userProfile.realName;
+    const solvedProblems =response.data.data.matchedUser.submitStats.acSubmissionNum;
+
+    const easy = solvedProblems.find((d) => d.difficulty === "Easy").count;
+    const medium = solvedProblems.find((d) => d.difficulty === "Medium").count;
+    const hard = solvedProblems.find((d) => d.difficulty === "Hard").count;
+
+    const result = {
+      userName: realName,
+      profilePic: userAvatar,
+      rank: userRanking,
+      solved: {
+        easy: easy,
+        medium: medium,
+        hard: hard,
+      },
+    };
+
+    res.status(200).json(result);
+  } catch (error) {
+    console.error(
+      "Error fetching data from LeetCode:",
+      error.response ? error.response.data : error.message
+    );
+    res.status(500).json({ error: "Error fetching LeetCode data" });
+  }
+});
+
+app.post("/dsa", async (req, res) => {
+  const newStructure = new DSA(req.body);
+  try {
+    await newStructure.save();
+    res.status(201).json(newStructure);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+app.get("/dsa/:name", async (req, res) => {
+  try {
+    const item = await DSA.findOne({ dsaName: req.params.name });
+    if (!item) {
+      return res.status(404).json({ message: "Data structure not found" });
+    }
+    res.status(200).json(item);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.post("/time", async (req, res) => {
+  const newComplexity = new Time(req.body);
+  try {
+    await newComplexity.save();
+    res.status(201).json(newComplexity);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+
+app.get("/time/:name", async (req, res) => {
+  try {
+    const item = await Time.findOne({ dsaName: req.params.name });
+    if (!item) {
+      return res.status(404).json({ message: "Data structure not found" });
+    }
+    res.status(200).json(item);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
