4ee5ea163c30e74a40ed9418e400f846bd054b8f
ishantgandhi <126364217+ikenakami@users.noreply.github.com>
2024-09-16 18:33:43 +0400

    base setup of react native

---
+++ hash/app/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+
+const index = () => {
+  return (
+    <View >
+      <Text>index</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({});
