cf62da44b7d8c6394b316d7b5aadd725097d5f18
ishantgandhi <126364217+ikenakami@users.noreply.github.com>
2024-09-17 00:34:26 +0400

    made splash screen

--- hash/app/index.jsx
+++ hash/app/index.jsx
@@ -1,16 +1,26 @@
-import { Text, View } from 'react-native';
+import { Image, ScrollView, Text, View } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
+import { LinearGradient } from "expo-linear-gradient";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const index = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pthin">index</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient
+      colors={["#FFFFFF", "#1CBABA"]}
+      start={{ x: 0.05, y: 0.1 }}
+      end={{ x: 0, y: 1.5 }}
+      className="h-full"
+    >
+      <SafeAreaView className="h-full">
+        <ScrollView contentContainerStyle={{height:"100%"}}>
+          <View className=" h-full justify-center items-center">
+            <Image source={require("../assets/images/hlogo.png")} />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
 export default index;
-
-
