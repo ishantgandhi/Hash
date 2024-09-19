7696691166cd2ca673db601966df965e302660a0
ishantgandhi <126364217+ikenakami@users.noreply.github.com>
2024-09-20 01:27:18 +0400

    made navbar component

---
+++ hash/app/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import { Image, ScrollView, Text, View, TextInput } from "react-native";
+import React from "react";
+import AppIcon from "../svg/AppIcon";
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+const navbar = () => {
+  return (
+        <View className="flex-row justify-between items-center h-20 w-full mb-3 px-5">
+          <AppIcon width="70px" />
+          <AntDesign name="logout" size={40} color="black" />
+        </View>
+  );
+};
+
+export default navbar;
--- hash/app/index.jsx
+++ hash/app/index.jsx
@@ -1,27 +1,15 @@
 import { Image, ScrollView, Text, View, TextInput } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { LinearGradient } from "expo-linear-gradient";
 import { SafeAreaView } from "react-native-safe-area-context";
-import AppIcon from "./svg/AppIcon";
-
+import NavBar from "./components/NavBar";
 const index = () => {
   return (
     <SafeAreaView className="flex-1">
       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-        <View className="flex-1 justify-center items-center px-2">
-          <View className="justify-center items-center mb-5 w-full">
-            <AppIcon width="160" height="160" />
-            <TextInput
-              className="border border-gray-400 rounded-lg w-5/6 h-16 mt-5 px-5"
-              placeholder="Enter Leetcode Username"
-            />
-          </View>
-        </View>
-        <View className="w-5/6 mb-10 px-2 self-center">
-          <View className=" h-16 bg-primary border rounded-xl justify-center items-center flex-row ">
-            <Text className="text-xl font-pmedium">Login</Text>
-          </View>
+        <NavBar />
+        <View className="flex-1 justify-center items-center p-5 border">
+          <View className="justify-center items-center h-full w-full border"></View>
         </View>
       </ScrollView>
     </SafeAreaView>
