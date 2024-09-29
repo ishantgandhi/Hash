c20a0e02745bb155fc7d760bc717811f5c3ba7f8
ishantgandhi <126364217+ikenakami@users.noreply.github.com>
2024-09-29 14:51:01 -0700

    Implemented Splash Screen

--- hash/app/index.jsx
+++ hash/app/index.jsx
@@ -1,10 +1,33 @@
-import { Image, ScrollView, Text, View, TextInput } from "react-native";
-import React from "react";
+import { Animated, View } from "react-native";
+import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Link } from 'expo-router';
 import Splash from "./splash.jsx";
 
 const Index = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const fadeAnim = useState(new Animated.Value(1))[0];
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start(() => setIsLoading(false));
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [fadeAnim]);
+
+  if (isLoading) {
+    return (
+      <Animated.View style={{ flex: 1, opacity: fadeAnim }}>
+        <Splash />
+      </Animated.View>
+    );
+  }
+
   return (
     <View className="flex-1 items-center justify-center">
       <StatusBar style="auto" />
--- hash/app/splash.jsx
+++ hash/app/splash.jsx
@@ -1,27 +1,27 @@
-// import { Image, ScrollView, Text, View } from "react-native";
-// import React from "react";
-// import { StatusBar } from "expo-status-bar";
-// import { LinearGradient } from "expo-linear-gradient";
-// import { SafeAreaView } from "react-native-safe-area-context";
-// import Hlogo from "./svg/Hlogo";
+import { Image, ScrollView, Text, View } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { LinearGradient } from "expo-linear-gradient";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Hlogo from "./svg/Hlogo";
 
-// const Splash = () => {
-//   return (
-//     <LinearGradient
-//       colors={["#B084FC", "#E6D9FF"]}
-//       start={{ x: 0.05, y: 0.1 }}
-//       end={{ x: 0, y: 1.5 }}
-//       className="h-full"
-//     >
-//       <SafeAreaView className="h-full">
-//         <ScrollView contentContainerStyle={{height:"100%"}}>
-//           <View className=" h-full justify-center items-center">
-//             <Hlogo />
-//           </View>
-//         </ScrollView>
-//       </SafeAreaView>
-//     </LinearGradient>
-//   );
-// };
+const Splash = () => {
+  return (
+    <LinearGradient
+      colors={["#B084FC", "#E6D9FF"]}
+      start={{ x: 0.05, y: 0.1 }}
+      end={{ x: 0, y: 1.5 }}
+      className="h-full w-full"
+    >
+      <SafeAreaView className="h-full">
+        <ScrollView contentContainerStyle={{height:"100%"}}>
+          <View className=" h-full justify-center items-center">
+            <Hlogo />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </LinearGradient>
+  );
+};
 
-// export default Splash;
+export default Splash;
