3ce499645f3ddf7ba1e68f96c05cf39f787e8c03
ishantgandhi <126364217+ikenakami@users.noreply.github.com>
2024-10-04 01:02:11 -0700

    created popular alogorithm screen

--- hash/app/algos.jsx
+++ hash/app/algos.jsx
@@ -1,19 +1,49 @@
 import { Text, View, FlatList, TouchableOpacity, Linking } from "react-native";
 import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import Ionicons from "@expo/vector-icons/Ionicons";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { useNavigation } from "@react-navigation/native"; // Import useNavigation hook
 
 const Algos = () => {
+  const [algos, setAlgos] = useState([]);
+  const navigation = useNavigation(); 
+  useEffect(() => {
+    const fetchAlgos = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/algo/getAll");
+        const data = await response.json();
+        setAlgos(data);
+      } catch (error) {
+        console.error("Error fetching algorithms:", error);
+      }
+    };
+
+    fetchAlgos();
+  }, []);
+
+  const renderAlgoItem = ({ item }) => (
+    <TouchableOpacity onPress={() => Linking.openURL(item.algoLink)}>
+      <View className="p-4 mb-3 bg-h300 rounded-2xl flex-row justify-between items-center">
+        <View>
+          <Text className="text-xl font-psemibold text-h800">{item.name}</Text>
+        </View>
+        <AntDesign name="link" size={30} color="#5921a8" />
+      </View>
+    </TouchableOpacity>
+  );
+
   return (
     <SafeAreaView className="flex-1">
-      <View className="flex-1 px-5 py-7 items-center justify-center">
-        <Ionicons name="construct" size={200} color="#e6d9ff" />
-        <Text className="mt-5 text-2xl font-pregular text-h400">
-          Under Construction
-        </Text>
-        <Text className="text-lg font-plight text-h400">
-          Please Check back soon
-        </Text>
+      <View className="flex-1 px-5 py-7">
+        <View className="flex-row justify-between items-center mb-7">
+          <Text className="text-3xl font-psemibold">
+            Popular Algorithms
+          </Text>
+          <TouchableOpacity onPress={() => navigation.navigate("home")}>
+            <AntDesign name="arrowleft" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
+        <FlatList data={algos} renderItem={renderAlgoItem} />
       </View>
     </SafeAreaView>
   );
