5546a3a7a0c950252f872bfdd3f09455527ed912
ishantgandhi <126364217+ikenakami@users.noreply.github.com>
2024-10-03 17:50:06 -0700

    changes position of navbar

--- hash/app/(tabs)/_layout.jsx
+++ hash/app/(tabs)/_layout.jsx
@@ -15,7 +15,7 @@ const TabsLayout = () => {
           tabBarInactiveTintColor: "#b084fc",
           tabBarStyle: {
             position: "absolute",
-            bottom: 40,
+            bottom: 45,
             left: 20,
             right: 20,
             borderRadius: 20,
