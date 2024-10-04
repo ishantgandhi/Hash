42fcd80a4112398ed80c61937a21d4111252ff17
ishantgandhi <126364217+ikenakami@users.noreply.github.com>
2024-10-04 01:00:42 -0700

    removed extra stack screen from (tabs)  _layout

--- hash/app/(tabs)/one/_layout.jsx
+++ hash/app/(tabs)/one/_layout.jsx
@@ -8,7 +8,6 @@ const _layout = () => {
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="dsaitem" options={{ headerShown: false }} /> 
       <Stack.Screen name="complexity" options={{ headerShown: false }} /> 
-      <Stack.Screen name="algos" options={{ headerShown: false }} /> 
     </Stack>
   );
 };
